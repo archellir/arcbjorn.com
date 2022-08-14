8a7bf311110b80207c9cc39ef9ca6876b0361083
arcbjorn <arky.happ@gmail.com>
2022-08-15 01:05:59 +0400

    feat (Tailwind): extend tailwind theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}",
   ],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ibm: ["IBM Plex Mono", "font-mono"],
+      },
+      height: {
+        "1/10": "10%",
+        "9/10": "90%",
+      },
+    },
   },
   plugins: [],
 };
