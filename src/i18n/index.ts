3bbd4be9ec75de0fe356893906af07f70fd1ee26
arcbjorn <arky.happ@gmail.com>
2022-08-14 22:28:43 +0400

    feat (i18n): add language detection logic

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,5 +1,7 @@
+import Cookies from "js-cookie";
 import { createI18nContext } from "@solid-primitives/i18n";
 import { ELanguage, Translations } from "@/i18n/types";
+import { getBrowserLocale, isTranslatableLanguage } from "@/helpers/i18n";
 
 import en from "@/i18n/translations/en.json";
 import de from "@/i18n/translations/de.json";
@@ -7,7 +9,9 @@ import es from "@/i18n/translations/es.json";
 import ru from "@/i18n/translations/ru.json";
 import se from "@/i18n/translations/se.json";
 
-const locales: Record<ELanguage, Translations> = {
+const LANG_COOKIE_NAME = "arc_lang";
+
+const translations: Record<ELanguage, Translations> = {
   [ELanguage.EN]: en,
   [ELanguage.DE]: de,
   [ELanguage.ES]: es,
@@ -15,6 +19,18 @@ const locales: Record<ELanguage, Translations> = {
   [ELanguage.SE]: se,
 };
 
-const translationsContext = createI18nContext(locales, ELanguage.EN);
+let language = Cookies.get(LANG_COOKIE_NAME);
+
+if (!language || !isTranslatableLanguage(language)) {
+  const browserLanguage = getBrowserLocale(true);
+
+  language = isTranslatableLanguage(browserLanguage)
+    ? browserLanguage
+    : ELanguage.EN;
+
+  Cookies.set(LANG_COOKIE_NAME, language, { sameSite: "strict" });
+}
+
+const translationsContext = createI18nContext(translations, language);
 
 export default translationsContext;
