80b0717aa8fe65f538f1b52060f2367f5fdbc1af
arcbjorn <arky.happ@gmail.com>
2022-08-14 21:20:46 +0400

    feat (i18n): provide translations to App

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,14 @@ import "@/index.css";
 import { render } from "solid-js/web";
 
 import App from "@/App";
+import { I18nContext } from "@solid-primitives/i18n";
+import translations from "@/i18n";
 
-render(() => <App />, document.getElementById("root") as HTMLElement);
+render(
+  () => (
+    <I18nContext.Provider value={translations}>
+      <App />
+    </I18nContext.Provider>
+  ),
+  document.getElementById("root") as HTMLElement
+);
