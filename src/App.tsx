05cca3419499ad32f98cffabfb4d3c280cda1c6f
arcbjorn <arky.happ@gmail.com>
2022-08-14 23:14:51 +0400

    feat (App): use router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import type { Component } from 'solid-js';
+import { Component, lazy } from "solid-js";
+import { Route, Router, Routes } from "@solidjs/router";
+
+const Home = lazy(() => import("@/pages"));
 
 const App: Component = () => {
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+    <Routes>
+      <Route path="/" component={Home} />
+    </Routes>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 /* @refresh reload */
 import "@/index.css";
 import { render } from "solid-js/web";
+import { Router } from "@solidjs/router";
+import { I18nContext } from "@solid-primitives/i18n";
 
 import App from "@/App";
-import { I18nContext } from "@solid-primitives/i18n";
 import translations from "@/i18n";
 
 render(
   () => (
     <I18nContext.Provider value={translations}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </I18nContext.Provider>
   ),
   document.getElementById("root") as HTMLElement
