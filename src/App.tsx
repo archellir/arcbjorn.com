aad41285219e82f039d669df816bc0c78df3a0dc
arcbjorn <arky.happ@gmail.com>
2022-08-22 01:03:25 +0400

    ref (App): styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import { Component, lazy } from "solid-js";
-import { Route, Router, Routes } from "@solidjs/router";
+import { Route, Routes } from "@solidjs/router";
+
+import Header from "@/components/header/Header";
 
 const Terminal = lazy(() => import("@/components/terminal/Terminal"));
 
 const App: Component = () => {
   return (
-    <Routes>
-      <Route path="/" component={Terminal} />
-    </Routes>
+    <>
+      <div class="h-1/10">
+        <Header />
+      </div>
+      <div class="h-5/6">
+        <Routes>
+          <Route path="/" component={Terminal} />
+        </Routes>
+      </div>
+    </>
   );
 };
 
